db0eee0ca16e4d1b8ccb6f7b707bc2d5dd6433be
Drew Wills <wills.drew@gmail.com>
2018-08-13 11:03:15 -0700

    Issue #82:  replace resource-server-webapp with a project freshly-generated from start.spring.io

---
+++ resource-server-webapp/src/main/java/org/jasig/portal/resourceserver/ResourceServerWebappApplication.java
@@ -0,0 +1,12 @@
+package org.jasig.portal.resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServerWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceServerWebappApplication.class, args);
+	}
+}
